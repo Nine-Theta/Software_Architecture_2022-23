4e44f0997a0065f48477018f043fc217f6d869b1
Nine <davidheetebrij@gmail.com>
2025-01-13 11:50:24 +0100

    updated billboard script to allow proper use with UI

--- Softarch TD/Assets/Scripts/SimpleBillboardScript.cs
+++ Softarch TD/Assets/Scripts/SimpleBillboardScript.cs
@@ -1,23 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// A simple script to be attached to any GameObject to make it look at the specified camera.
+/// A simple script to be attached to any GameObject to make it look at the specified transform.
 /// </summary>
 public class SimpleBillboardScript : MonoBehaviour
 {
-    [SerializeField]
-    private Camera _targetCamera;
+    [SerializeField, Tooltip("If left null, will use the main camera by default")]
+    private Transform _targetObject;
+
+    [SerializeField, Tooltip("Reverses the direction the billboard looks towards, used for world UI")]
+    private bool _invert;
 
     private void Start()
     {
-        if (_targetCamera == null)
-            _targetCamera = Camera.main;
+        if (_targetObject == null)
+            _targetObject = Camera.main.transform;
     }
 
     private void Update()
     {
-        transform.LookAt(_targetCamera.transform);
+        if (!_invert)
+            transform.LookAt(_targetObject.transform.position);
+        else
+            transform.LookAt(transform.position + (transform.position - _targetObject.transform.position));
     }
 }
